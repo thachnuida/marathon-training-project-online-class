1d946b43fd2abae08c67ef43c78b462846c34fec
quynhthuyen <quynhthuyentruong@outlook.com.vn>
2014-07-23 14:21:21 +0700

    Detele Project 'Cause Lack

--- online_class/class_management/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from class_management.models import *
-
-admin.site.register(Class)
-admin.site.register(Lesson)
-admin.site.register(Test)
-admin.site.register(Question)
--- online_class/home/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from django.contrib import admin
-from class_management.models import *
-
-admin.site.register(Teacher)
-admin.site.register(Student)
-
